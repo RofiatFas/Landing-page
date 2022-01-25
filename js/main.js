06ad61fa854d1cbc776f0c51f48fbe1347b0be0c
Rofiat Fasasi <usmanfasasirofiat@gmail.com>
2022-01-25 06:03:40 +0100

    changed text

--- js/main.js
+++ js/main.js
@@ -99,8 +99,8 @@ const sr = ScrollReveal({
 
 sr.reveal(`.home__data, .home__img,
             .about__data, .about__img,
-            .services__content, .menu__content,
-            .app__data, .app__img,
+            .services__content, .products__content,
+            .satisfaction__data, .satisfaction__img,
             .contact__data, .contact__button,
             .footer__content`, {
     interval: 200
